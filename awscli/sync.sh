213727d04176e4150e825632b5e6a48705f5eefe
asa511 <sayeed.anjum@gmail.com>
2015-11-23 22:16:15 +0530

    responding to signals

--- awscli/sync.sh
+++ awscli/sync.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 echo "Syncing to bucket $SYNC_S3_BUCKET using ID: $AWS_ACCESS_KEY_ID"
+
+trap "exit" SIGHUP SIGINT SIGTERM
+
 while true; do
    aws s3 sync --region $AWS_REGION /var/sync s3://$SYNC_S3_BUCKET
    sleep 60
